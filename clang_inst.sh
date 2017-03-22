fa79294bfbae3e30d9a3081481998add4c8ac52c
ilian <ilian@localhost.localdomain>
2017-03-22 16:29:22 +0200

    install clang if blender make_deps.sh fails

---
+++ clang_inst.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+ 
+SRCDIR="/opt/src"
+INSTDIR="/opt/llvm"
+ 
+echo ""
+ 
+if [ -d $SRCDIR/llvm/.svn ] ; then
+	echo "Updating "$SRCDIR"/llvm ..."
+	svn up $SRCDIR/llvm $SRCDIR/llvm/tools/clang
+else
+	echo "Checking out "$SRCDIR"/llvm ..."
+	svn checkout http://llvm.org/svn/llvm-project/llvm/trunk $SRCDIR/llvm
+	svn checkout http://llvm.org/svn/llvm-project/cfe/trunk $SRCDIR/llvm/tools/clang
+fi
+ 
+ 
+rm -rf $INSTDIR
+ 
+mkdir $SRCDIR/llvm_cmake
+cd $SRCDIR/llvm_cmake
+ 
+echo ""
+echo "Building..."
+ 
+cmake ../llvm \
+	-DCMAKE_INSTALL_PREFIX:STRING=$INSTDIR \
+	-DCMAKE_BUILD_TYPE:STRING=Debug \
+	-DLLVM_ENABLE_THREADS:BOOL=ON
+ 
+make -j6
+ 
+echo ""
+echo "Installing..."
+ 
+make install
+ 
+ 
+cp $SRCDIR/llvm/tools/clang/tools/scan-view/scan-view $INSTDIR/bin/
+cp $SRCDIR/llvm/tools/clang/tools/scan-view/*.py $INSTDIR/bin/
+ 
+cp $SRCDIR/llvm/tools/clang/tools/scan-build/scan-build $INSTDIR/bin/
+cp $SRCDIR/llvm/tools/clang/tools/scan-build/ccc-analyzer $INSTDIR/bin/
+cp $SRCDIR/llvm/tools/clang/tools/scan-build/c++-analyzer $INSTDIR/bin/
+cp $SRCDIR/llvm/tools/clang/tools/scan-build/sorttable.js $INSTDIR/bin/
+cp $SRCDIR/llvm/tools/clang/tools/scan-build/scanview.css $INSTDIR/bin/
+ 
+echo ""
+echo "Done!"
