ff3e7976afee5f106a0c38047700be7675564d90
heatblazer <ilianzapryanov@abv.bg>
2017-06-01 11:39:32 +0300

    more work

--- MouseTo3D.py
+++ MouseTo3D.py
@@ -1,7 +1,9 @@
 import bpy
 import bgl
+import bmesh
 from bpy_extras import view3d_utils
 import mathutils
+from mathutils import Vector
 
 """Functions for the mouse_coords_to_3D_view"""
 def get_viewport():
@@ -26,40 +28,50 @@ def get_projection_matrix():
 def mouse_coords_to_3D_view(x, y):
     depth = bgl.Buffer(bgl.GL_FLOAT, [0.0])
     bgl.glReadPixels(x, y, 1, 1, bgl.GL_DEPTH_COMPONENT, bgl.GL_FLOAT, depth)
-    if (depth[0] != 1.0):
-        world_x = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
-        world_y = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
-        world_z = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
-        view1 = get_viewport()
-        model = get_modelview_matrix()
-        proj = get_projection_matrix ()
-        bgl.gluUnProject(x, y, depth[0],
-                         model, proj,
-                         view1,
-                         world_x, world_y, world_z)
-    return world_x[0], world_y[0], world_z[0]
-
+    world_x = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
+    world_y = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
+    world_z = bgl.Buffer(bgl.GL_DOUBLE, 1, [0.0])
+    view1 = get_viewport()
+    model = get_modelview_matrix()
+    proj = get_projection_matrix ()
+    bgl.gluUnProject(x, y, depth[0],
+                     model, proj,
+                     view1,
+                     world_x, world_y, world_z)
+    return float(world_x[0]), float(world_y[0]), float(world_z[0])
 
 """drawing point OpenGL in mouse_coords_to_3D_view"""
 def draw_callback_px(self, context):
     # mouse coordinates relative to 3d view
-    x, y= self.mouse_path
+    x, y = self.mouse_path
+    mx, my = self.mx, self.my #got from modal
 
     # mouse coordinates relative to Blender interface
     view = get_viewport()
     gmx = view[0] + x
     gmy = view[1] + y
 
-    #c= bgl.Buffer(bgl.GL_UNSIGNED_BYTE, [3,1])
-    #bgl.glReadPixels(x, y, 1, 1, bgl.GL_RGB, bgl.GL_FLOAT, c)
-    #c= bgl.Buffer(bgl.GL_SHORT, [3,1])
-    c = bgl.Buffer(bgl.GL_FLOAT, [3,1])
-    bgl.glReadPixels(gmx, gmy,1,1,bgl.GL_RGB,bgl.GL_FLOAT,c);
+    if False:
+        #c= bgl.Buffer(bgl.GL_UNSIGNED_BYTE, [3,1])
+        #bgl.glReadPixels(x, y, 1, 1, bgl.GL_RGB, bgl.GL_FLOAT, c)
+        #c= bgl.Buffer(bgl.GL_SHORT, [3,1])
+        c = bgl.Buffer(bgl.GL_FLOAT, [3,1])
+        bgl.glReadPixels(gmx, gmy,1,1,bgl.GL_RGB,bgl.GL_FLOAT,c);
+        draw_square_follow_cursor(c, gmx, gmy)
+    else:
+        draw_uv_sphere(mx, my, 0.5)
 
 
-    draw_square_follow_cursor(c, gmx, gmy)
-    #draw_corner_square(c)
-    draw_square(c)
+def draw_uv_sphere(mx, my, s):
+    print("Draw uv sphere")
+    ob = bpy.data.objects['Cube']
+    v = ob.data.vertices[0].co
+    mat = ob.matrix_world
+    x,y,z = mouse_coords_to_3D_view(mx, my)
+    mv = Vector((x, y, z))
+    print(mv)
+    bpy.ops.mesh.primitive_uv_sphere_add(segments=42, ring_count=42, location=mat * mv, size=s)
+
 
 def draw_square_follow_cursor(c, gmx, gmy):
 
@@ -68,9 +80,9 @@ def draw_square_follow_cursor(c, gmx, gmy):
     bgl.glColor3f(0.0,0.0,255.0)
     bgl.glPointSize(30)
     bgl.glBegin(bgl.GL_POINTS)
-    #bgl.glVertex3f(*(mouse3d))
+    bgl.glVertex3f(*(mouse3d))
     bgl.glVertex3f(mouse3d[0], mouse3d[1], mouse3d[2])
-    #bgl.glVertex2f(gmx,gmy)
+    bgl.glVertex2f(gmx,gmy)
     bgl.glEnd()
 
     # restore opengl defaults
@@ -123,16 +135,11 @@ class ModalDrawOperator(bpy.types.Operator):
     def modal(self, context, event):
         context.area.tag_redraw()
 
-
         if event.type == 'MOUSEMOVE':
             self.mouse_path = (event.mouse_region_x, event.mouse_region_y)
-
         elif event.type == 'LEFTMOUSE':
-            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
-            context.area.header_text_set()
-            return {'FINISHED'}
-
-
+            self.mx, self.my= event.mouse_x, event.mouse_y
+            return {'PASS_THROUGH'}
         elif event.type in {'RIGHTMOUSE', 'ESC'}:
             bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             context.area.header_text_set()
